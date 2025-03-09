32aa51000a8f2d39dbc3c729b724697548cfda1f
A13XBGA <alexandru.adrian@mail.mcgill.ca>
2025-03-09 17:51:03 -0400

    edited code to test serial connection instead of SPI using sample test code

--- flight-computer-software/test/test_simpler_comm_control/test_simpler_comm_control.cpp
+++ flight-computer-software/test/test_simpler_comm_control/test_simpler_comm_control.cpp
@@ -1,39 +1,123 @@
-#include "SPI.h"
+#include <Arduino.h>
+// Serial test code 
+// Pins to use for serial:
+// Teensy 1 TX1 → Teensy 2 RX1
+// Teensy 1 RX1 → Teensy 2 TX1
+// Teensy 1 GND → Teensy 2 GND
 
-#include "SPI_MSTransfer_MASTER.h"
-SPI_MSTransfer_MASTER<&SPI, 10, 0x1234> mySPI1234;
-SPI_MSTransfer_MASTER<&SPI, 10, 0x4567> mySPI4567;
+// Source for test code: https://forum.pjrc.com/index.php?threads/serial-communication-between-two-teensy-4-1.71093/
+/************************
+     Send and Receive Code
+************************/
+
+#define RxD Serial7
+#define TxD Serial6
+
+#define baud 230400 //115200
+
+#pragma pack(push,1)
+
+/* Data Type to be sent received */
+struct dataType {
+    uint32_t dataIdx;
+    float vPos, hPos, vCurrent, hCurrent;
+};
+
+dataType tx, rx;   // Variables to hold the data for Send/Receive
+#pragma pack(pop)
+
+//placeholder for information getting sent
+float ivpos = 0.10, ihpos = 0.20, ivcurrent = 0.0, ihcurrent = 0.0;
+float avpos = 0.50, ahpos = 0.70, avcurrent = 0.29, ahcurrent = 0.35;
+
+void ClearSerialBuffers() {
+    TxD.flush();
+    RxD.clear();
+    delay(1000);
+    TxD.flush();
+    RxD.clear();
+}
 
 void setup() {
-  Serial.begin(115200);
-  SPI.begin();
-  pinMode(10, OUTPUT);
-  digitalWrite(10, 1);
-  mySPI1234.begin();
-  mySPI4567.begin();
+
+    // Initialize serial interface
+    Serial.begin(9600);
+    RxD.begin(baud);
+    TxD.begin(baud);
+
+    pinMode(LED_BUILTIN, OUTPUT);
+    // Wait until the serial interfaces are active
+    while (!Serial && millis() < 5000);  // Wait for Serial (USB) to become active
+    Serial.println("Serial ports active:");
+    Serial.println("     Sending on Serial6 Tx, Receiving on Serial 7 Rx");
+
+    tx.dataIdx = 0;
+    ClearSerialBuffers();
+    delay(1000);    // Just to allow synch time between T4.1s
 }
+
+elapsedMillis timeout;
+#define waitTime 5000
 
 void loop() {
 
-  static uint32_t t = millis();
-  if ( millis() - t > 1000 ) {
-    Serial.println(millis());
+    tx.vPos = avpos;
+    tx.hPos = ahpos;
+    tx.vCurrent = avcurrent;
+    tx.hCurrent = ahcurrent;
+    tx.dataIdx++;
 
-    uint16_t buf[5] = { 0xF1, 0xF2, 0xDEAD, 0xF4, 0xBEEF };
-    uint16_t buf2[5] = { 0xBEEF, 0xF7, 0xF8, 0xF9, 0xDEAD };
-    mySPI1234.transfer16(buf2, 5, random(0x1000, 0x8000));
-    mySPI4567.transfer16(buf, 5, random(0x1000, 0x8000));
+    /*  SendPacket */
+    digitalWriteFast(LED_BUILTIN, HIGH);
+    TxD.write((uint8_t*)&tx, sizeof(tx));    //Send the data
+    TxD.flush();
 
-    static bool flip = 0;
-    flip = !flip;
-    mySPI1234.digitalWrite(6, flip);
-    //    mySPI1234.pinMode(5, INPUT);
-    bool moo = mySPI1234.digitalRead(6);
-    Serial.print("State: "); Serial.println(moo);
-    mySPI1234.detectSlaves();
+    digitalWriteFast(LED_BUILTIN, LOW);
 
-    mySPI1234.pinMode(5, INPUT);
-    t = millis();
-  }
+    Serial.print("Sent packet ---->  ");
+    Serial.print(tx.dataIdx);
+    Serial.print(" , ");
+    Serial.print(tx.vPos);
+    Serial.print(" , ");
+    Serial.print(tx.hPos);
+    Serial.print(" , ");
+    Serial.print(tx.vCurrent);
+    Serial.print(" , ");
+    Serial.print(tx.hCurrent);
+    Serial.println();
 
+    timeout = 0;
+
+    while ((RxD.available() < sizeof(rx)) && (timeout <= waitTime)) {}       // Wait for complete data received
+    /*        ReceivePacket */
+    if (timeout >= waitTime) {
+        Serial.println("Timeout waiting for Rx Data");
+        ClearSerialBuffers();
+    }
+    else {
+        rx.dataIdx = 0;
+        rx.hCurrent = 0;
+        rx.hPos = 0;
+        rx.vCurrent = 0;
+        rx.vPos = 0;
+
+        RxD.readBytes((uint8_t*)&rx, sizeof(rx));   // Get the data from Serial Buffer
+        Serial.print("Read packet ---->  ");
+        Serial.print(rx.dataIdx);
+        Serial.print(" , ");
+        Serial.print(rx.vPos);
+        Serial.print(" , ");
+        Serial.print(rx.hPos);
+        Serial.print(" , ");
+        Serial.print(rx.vCurrent);
+        Serial.print(" , ");
+        Serial.print(rx.hCurrent);
+        Serial.println();
+
+        if (rx.dataIdx != tx.dataIdx) {
+            Serial.println("Lost Packet");
+            ClearSerialBuffers();
+        }
+
+    }
 }
--- flight-computer-software/test/test_simpler_comm_power/test_simpler_comm_power.cpp
+++ flight-computer-software/test/test_simpler_comm_power/test_simpler_comm_power.cpp
@@ -1,25 +1,68 @@
-#include "SPI_MSTransfer_T4.h"
-SPI_MSTransfer_T4<&SPI, 0x1234> mySPI;
+#include <Arduino.h>
+
+// Source for test code: https://forum.pjrc.com/index.php?threads/serial-communication-between-two-teensy-4-1.71093/
+/*****************************
+  Receive and Echo Back Code
+******************************/
+// Teensy 1 TX1 → Teensy 2 RX1
+// Teensy 1 RX1 → Teensy 2 TX1
+// Teensy 1 GND → Teensy 2 GND
+
+#define RxD Serial7
+#define TxD Serial6
+#pragma pack(push,1)
+
+/* Data Type to be sent received */
+struct dataType {
+    uint32_t dataIdx;
+    float vPos, hPos, vCurrent, hCurrent;
+};
+
+dataType rx;   // Variables to hold the data for Send/Receive
+#pragma pack(pop)
+
+#define baud 230400  //115200
+
+void ClearSerialBuffers() {
+    TxD.flush();
+    RxD.clear();
+    delay(1000);
+    TxD.flush();
+    RxD.clear();
+}
 
 void setup() {
-  Serial.begin(115200);
-  mySPI.begin();
-  mySPI.onTransfer(myCB);
-}
 
+    // Initialize serial interface
+    Serial.begin(9600);
+    RxD.begin(baud);
+    TxD.begin(baud);
+
+    pinMode(LED_BUILTIN, OUTPUT);
+    // Wait until the serial interfaces are active
+    while (!Serial && millis() < 5000);  // Wait for Serial (USB) to become active
+    Serial.println("Serial ports active:");
+    Serial.println("     Receiving on Serial7 Rx, Sending on Serial 6 Tx");
+
+    ClearSerialBuffers();
+
+}
 void loop() {
-  mySPI.events();
-  static uint32_t t = millis();
-  if ( millis() - t > 1000 ) {
-    Serial.print("millis: "); Serial.println(millis());
-    t = millis();
-  }
-}
 
-void myCB(uint16_t *buffer, uint16_t length, AsyncMST info) {
-  for ( int i = 0; i < length; i++ ) {
-    Serial.print(buffer[i], HEX); Serial.print(" ");
-  }
-  Serial.print(" --> Length: "); Serial.print(length);
-  Serial.print(" --> PacketID: "); Serial.println(info.packetID, HEX);
+    if (RxD.available() >= sizeof(rx)){
+ 
+    /*        ReceivePacket */
+        digitalWriteFast(LED_BUILTIN, HIGH);
+        rx.dataIdx = 0;
+        rx.hCurrent = 0;
+        rx.hPos = 0;
+        rx.vCurrent = 0;
+        rx.vPos = 0;
+        RxD.readBytes((uint8_t*)&rx, sizeof(rx));   // Get the data from Serial Buffer
+        /*  SendPacket */
+        TxD.write((uint8_t*)&rx, sizeof(rx));    //Send the data
+        TxD.flush();
+        digitalWriteFast(LED_BUILTIN, LOW);
+
+    }
 }
