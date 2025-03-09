2e3bbea7610208169713c2f3d93fec1320bedf48
A13XBGA <112523942+A13XBGA@users.noreply.github.com>
2025-03-08 20:49:04 -0500

    updated comm control test code

--- flight-computer-software/test/test_simpler_comm_control/test_simpler_comm_control.cpp
+++ flight-computer-software/test/test_simpler_comm_control/test_simpler_comm_control.cpp
@@ -7,17 +7,20 @@ void setup() {
     pinMode(mosi, OUTPUT);
     pinMode(miso, INPUT);
     pinMode(sck, OUTPUT);
-    // initialize SPI:
-    SPI.begin(); 
+    // Initialize SPI
+    SPI.begin();
 }
 
 void loop() {
     // Assert Chip Select
+    // Teensy 4.1 is ARM-based, thus uses little-endian byte order
+    SPI.beginTransaction(SPISettings(1000000, LSBFIRST, SPI_MODE0));
     digitalWrite(cs, LOW);
     // Creating loop to send numbers in increasing order
     for (int i = 0; i < 100; i++) {
         // Send data
         SPI.transfer(uint8_t(i));
+        Serial.println(i);
         delay(500);
     }
 }
